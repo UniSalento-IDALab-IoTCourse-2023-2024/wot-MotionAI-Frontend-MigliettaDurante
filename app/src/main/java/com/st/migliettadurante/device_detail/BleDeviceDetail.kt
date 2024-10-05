4c9dde6c4b0a85bb4925eaaedbfeee88150da7e6
Sam100801 <samuel.miglietta@libero.it>
2024-10-05 17:29:28 +0200

    risoluzione bug pulsante "indietro" schermata BleDeviceDetail.kt

--- app/src/main/java/com/st/migliettadurante/device_detail/BleDeviceDetail.kt
+++ app/src/main/java/com/st/migliettadurante/device_detail/BleDeviceDetail.kt
@@ -40,12 +40,21 @@ fun BleDeviceDetail(
         viewModel.connect(deviceId = deviceId)
     }
 
+    // Stati per monitorare se il dispositivo e le funzionalità sono stati caricati
+    var isDeviceLoaded by remember { mutableStateOf(false) }
+    var isFeaturesLoaded by remember { mutableStateOf(false) }
+
     val bleDevice = viewModel.bleDevice(deviceId = deviceId).collectAsState(null)
     val features = viewModel.features.collectAsState()
 
+    // Verifica se il dispositivo è pronto e carica le funzionalità
     if (bleDevice.value?.connectionStatus?.current == NodeState.Ready) {
         viewModel.getFeatures(deviceId = deviceId)
+        isDeviceLoaded = true // Imposta il dispositivo come caricato
     }
+
+    // Imposta le funzionalità come caricate se non è vuoto
+    isFeaturesLoaded = features.value.isNotEmpty()
 
     val backHandlingEnabled by remember { mutableStateOf(true) }
 
@@ -66,171 +75,181 @@ fun BleDeviceDetail(
             )
     ) {
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            if (viewModel.showAudioBtn(deviceId)) {
-                Text(
-                    text = stringResource(R.string.st_testAudio),
-                    color = Color(0xFF007AFF),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                IconButton(
-                    enabled = bleDevice.value?.connectionStatus?.current == NodeState.Ready,
-                    onClick = {
-                        navController.navigate("audio/$deviceId")
-                    }
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Send,
-                        contentDescription = null,
-                        tint = Color(0xFF007AFF)
-                    )
-                }
-            }
-        }
-
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            shape = RoundedCornerShape(12.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.White)
-        ) {
+        // Caricamento del dispositivo
+        if (isDeviceLoaded) {
             Row(
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_device),
-                    contentDescription = "Device Icon",
-                    tint = Color(0xFF3B82F6),
-                    modifier = Modifier.size(32.dp)
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column {
+                if (viewModel.showAudioBtn(deviceId)) {
                     Text(
-                        text = "Dispositivo collegato:",
-                        style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
-                        color = Color(0xFF374151)
+                        text = stringResource(R.string.st_testAudio),
+                        color = Color(0xFF007AFF),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold
                     )
-                    Text(
-                        text = bleDevice.value?.device?.name ?: "N/A",
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.Bold,
-                        color = Color(0xFF6B7280)
-                    )
-                }
-            }
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            shape = RoundedCornerShape(12.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.White)
-        ) {
-            Row(
-                modifier = Modifier.padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_status),
-                    contentDescription = "Status Icon",
-                    tint = Color(0xFF10B981),
-                    modifier = Modifier.size(32.dp)
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column {
-                    Text(
-                        text = "Stato attuale:",
-                        style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
-                        color = Color(0xFF374151)
-                    )
-                    Text(
-                        text = bleDevice.value?.connectionStatus?.current?.name?.uppercase() ?: "DISCONNECTED",
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Color(0xFF6B7280)
-                    )
-                }
-            }
-        }
-
-        Text(
-            text = "Funzioni disponibili:",
-            style = MaterialTheme.typography.titleMedium,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 20.sp,
-            color = Color.Black,
-            modifier = Modifier
-                .padding(vertical = 8.dp)
-                .padding(horizontal = 16.dp)
-        )
-
-        val filteredFeatures = features.value.filter {
-            it.name == "Accelerometer" || it.name == "Activity Recognition"
-        }
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(1f)
-                .padding(horizontal = 10.dp),
-            contentPadding = PaddingValues(8.dp)
-        ) {
-            itemsIndexed(items = filteredFeatures) { index, item ->
-
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(70.dp)
-                        .padding(vertical = 6.dp, horizontal = 5.dp)
-                        .clickable {
-                            if (item.name == "Activity Recognition") {
-                                navController.navigate("feature/$deviceId/${item.name}/activity")
-                            } else {
-                                navController.navigate("feature/$deviceId/${item.name}")
-                            }
-                        },
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color(0xFFE3F2FD),
-                    ),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-                    shape = RoundedCornerShape(12.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.padding(12.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                    Spacer(modifier = Modifier.width(4.dp))
+                    IconButton(
+                        enabled = bleDevice.value?.connectionStatus?.current == NodeState.Ready,
+                        onClick = {
+                            navController.navigate("audio/$deviceId")
+                        }
                     ) {
                         Icon(
-                            painter = painterResource(id = R.drawable.ic_feature), // Icona per la feature
-                            contentDescription = "Feature Icon",
-                            tint = Color(0xFF007AFF),
-                            modifier = Modifier.size(24.dp)
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Text(
-                            text = item.name,
-                            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Medium)
+                            imageVector = Icons.Filled.Send,
+                            contentDescription = null,
+                            tint = Color(0xFF007AFF)
                         )
                     }
                 }
+            }
 
-                if (filteredFeatures.lastIndex != index) {
-                    Divider(color = Color(0xFFE5E7EB), thickness = 1.dp)
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ) {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_device),
+                        contentDescription = "Device Icon",
+                        tint = Color(0xFF3B82F6),
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column {
+                        Text(
+                            text = "Dispositivo collegato:",
+                            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
+                            color = Color(0xFF374151)
+                        )
+                        Text(
+                            text = bleDevice.value?.device?.name ?: "N/A",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFF6B7280)
+                        )
+                    }
                 }
             }
         }
-        if (filteredFeatures.isNotEmpty()){
+
+        // Caricamento delle funzionalità
+        if (isFeaturesLoaded) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ) {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_status),
+                        contentDescription = "Status Icon",
+                        tint = Color(0xFF10B981),
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column {
+                        Text(
+                            text = "Stato attuale:",
+                            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
+                            color = Color(0xFF374151)
+                        )
+                        Text(
+                            text = bleDevice.value?.connectionStatus?.current?.name?.uppercase() ?: "DISCONNECTED",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color(0xFF6B7280)
+                        )
+                    }
+                }
+            }
+
+            Text(
+                text = "Funzioni disponibili:",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .padding(horizontal = 16.dp)
+            )
+
+            val filteredFeatures = features.value.filter {
+                it.name == "Accelerometer" || it.name == "Activity Recognition"
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .padding(horizontal = 10.dp),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                itemsIndexed(items = filteredFeatures) { index, item ->
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(70.dp)
+                            .padding(vertical = 6.dp, horizontal = 5.dp)
+                            .clickable {
+                                if (item.name == "Activity Recognition") {
+                                    navController.navigate("feature/$deviceId/${item.name}/activity")
+                                } else {
+                                    navController.navigate("feature/$deviceId/${item.name}")
+                                }
+                            },
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(0xFFE3F2FD),
+                        ),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                        shape = RoundedCornerShape(12.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.padding(12.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_feature),
+                                contentDescription = "Feature Icon",
+                                tint = Color(0xFF007AFF),
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(
+                                text = item.name,
+                                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Medium)
+                            )
+                        }
+                    }
+
+                    if (filteredFeatures.lastIndex != index) {
+                        Divider(color = Color(0xFFE5E7EB), thickness = 1.dp)
+                    }
+                }
+            }
+        }
+
+        // Mostra il pulsante "Indietro" solo quando sia il dispositivo che le funzionalità sono stati caricati
+        if (isDeviceLoaded && isFeaturesLoaded) {
             Button(
-                onClick = { viewModel.disconnect(deviceId = deviceId);
+                onClick = {
+                    viewModel.disconnect(deviceId = deviceId)
                     navController.popBackStack()
                 },
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF2F3F4)),
@@ -244,7 +263,8 @@ fun BleDeviceDetail(
                     modifier = Modifier.size(16.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                Text("Indietro",
+                Text(
+                    text = "Indietro",
                     color = Color.Black,
                     fontSize = 16.sp,
                     fontWeight = FontWeight.SemiBold
